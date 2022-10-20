1f9ff33f58c006836b24f70bbf59ed7fcff57b1f
Irfan Ripat <irfanripat@gmail.com>
2022-10-20 22:19:09 +0800

    create composable search text field

---
+++ app/src/main/java/com/astro/test/irfan/SearchTextField.kt
@@ -0,0 +1,78 @@
+package com.astro.test.irfan
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.astro.test.irfan.ui.theme.GithubUsersTheme
+
+@Composable
+fun SearchTextField(
+    showClearIcon: Boolean = true,
+) {
+    var query: String by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = query,
+        onValueChange = { onQueryChanged ->
+            query = onQueryChanged
+            if (onQueryChanged.isNotEmpty()) {
+                // Perform search
+            }
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                tint = MaterialTheme.colors.onBackground,
+                contentDescription = "Search icon"
+            )
+        },
+        trailingIcon = {
+            if (showClearIcon) {
+                IconButton(onClick = { query = "" }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Clear,
+                        tint = MaterialTheme.colors.onBackground,
+                        contentDescription = "Clear icon"
+                    )
+                }
+            }
+        },
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+        placeholder = { Text(text = stringResource(R.string.hint_search_query)) },
+        textStyle = MaterialTheme.typography.subtitle1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.background, shape = RectangleShape)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GithubUsersTheme {
+        SearchTextField()
+    }
+}
