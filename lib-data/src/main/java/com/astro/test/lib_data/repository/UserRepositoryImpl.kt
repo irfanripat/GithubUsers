08ba41210b1e4f4bc146cc60f1060292ac3ef504
Irfan Ripat <irfanripat@gmail.com>
2022-10-22 17:06:31 +0800

    handle timeout and unknown hosting exception

--- lib-data/src/main/java/com/astro/test/lib_data/repository/UserRepositoryImpl.kt
+++ lib-data/src/main/java/com/astro/test/lib_data/repository/UserRepositoryImpl.kt
@@ -4,25 +4,33 @@ import com.astro.test.lib_data.model.SearchUserResponse
 import com.astro.test.lib_data.model.User
 import com.astro.test.lib_data.network.RetrofitHelper
 import retrofit2.Response
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 class UserRepositoryImpl : UserRepository {
 
     override suspend fun searchUsers(query: String): Response<SearchUserResponse> {
-        val users = RetrofitHelper.apiService.searchUsers(query)
-        return if (users.isSuccessful) {
-            val usersWithName = users.body()?.items?.map {
-                val user = getUser(it.login)
-                if (user.isSuccessful) {
-                    user.body()
-                } else {
-                    it
+        return try {
+            val response = RetrofitHelper.apiService.searchUsers("$query in:name")
+            if (response.isSuccessful) {
+                val usersWithName = response.body()?.items?.map {
+                    val user = getUser(it.login)
+                    if (user.isSuccessful) {
+                        user.body()
+                    } else {
+                        it
+                    }
                 }
+                usersWithName?.let {
+                    Response.success(SearchUserResponse(it.map { user -> user as User }))
+                } ?: Response.success(SearchUserResponse(emptyList()))
+            } else {
+                response
             }
-            usersWithName?.let {
-                Response.success(SearchUserResponse(it.map { user -> user as User }))
-            } ?: Response.success(SearchUserResponse(emptyList()))
-        } else {
-            users
+        } catch (e: UnknownHostException) {
+            Response.success(SearchUserResponse(emptyList()))
+        } catch (e: SocketTimeoutException) {
+            Response.success(SearchUserResponse(emptyList()))
         }
     }
 
